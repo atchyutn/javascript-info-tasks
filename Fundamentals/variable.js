edbca7c776da0a0856b6d7a7a689a7077f5285a9
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2022-04-05 00:25:31 +0530

    Added Variables

---
+++ Fundamentals/variable.js
@@ -0,0 +1,18 @@
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+alert(admin);
+
+
+let ourPlanet;
+ourPlanet = "The Earth";
+
+let currentUser;
+currentUser = "Atchyut";
+
+const BIRTHDAY = '18.04.1982'; // make uppercase
+
+const age = someCode(BIRTHDAY); 
